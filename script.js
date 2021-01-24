e195d52bc1bdf8a1b7f40626e852010a93c893d6
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-24 14:55:54 +0200

    conversion
    converted from document.write to adding elements

--- script.js
+++ script.js
@@ -79,7 +79,6 @@ let subject10={
 
 let arr=[subject1,subject2,subject3,subject4,subject5,subject6,subject7,subject8,subject9,subject10];
 
-function totalWork(){
 for(let sub of arr){
             let d=new Date(sub.finishedAt.getTime()-sub.startedAt.getTime());
             let str=d.getHours()-2;
@@ -91,7 +90,6 @@ for(let sub of arr){
             sub.tasksPercentage=Math.round((sub.tasksFinished/sub.tasksGiven)*100)+"%";
             
 }
-}
 
 // DOM manipulation
 const h1=document.createElement("h1");
@@ -102,8 +100,6 @@ document.body.append(h1);
 const table=document.createElement("table");
 table.id="table";
 document.body.append(table);
-addTitle();
-function addTitle(){
     const tr=document.createElement("tr");
     table.append(tr);
     for(let prop in arr[0]){
@@ -125,52 +121,56 @@ function addTitle(){
     td.setAttribute("class","title");
     td.innerHTML=str;
     }
-}
-
     
-//     document.write('<td class="'+temp+' title">'+str+'</td>');
-// }
 
-// for(let sub of arr){
-//     let cls="";
-//     document.write('<tr>');
-//     for(let prop in sub){
-//         cls="";
-//         if(prop==="tasksGiven"){
-//             let hrs=parseInt(sub["totalTimeSpent"]);
-//             if(hrs<=2)
-//                 cls="clg1";
-//             if(hrs>2&&hrs<=5)
-//                 cls="clg2";
-//             if(hrs>5)
-//                 cls="clg3";
-//         }
-//         if(prop==="tasksPercentage"){
-//             let per=parseInt(sub[prop].replace("%",""));
-//             if(per<=50)
-//                 cls="clp1";
-//             if(per>50&&per<=75)
-//                 cls="clp2";
-//             if(per>75)
-//                 cls="clp3";
-//         }
-//         if(prop==="startedAt"||prop==="finishedAt")
-//         {
-//             let date=String(sub[prop].getHours())+':'+String(sub[prop].getMinutes());
-//             if(sub[prop].getHours()>=0&&sub[prop].getHours()<=9)
-//                 date="0"+date;
+for(let sub of arr){
+    let cls="";
+    const tr=document.createElement("tr");
+    table.append(tr);
+    for(let prop in sub){
+        const td=document.createElement("td");
+        tr.append(td);
+        cls="";
+        if(prop==="tasksGiven"){
+            let hrs=parseInt(sub["totalTimeSpent"]);
+            if(hrs<=2)
+                cls="clg1";
+            if(hrs>2&&hrs<=5)
+                cls="clg2";
+            if(hrs>5)
+                cls="clg3";
+        }
+        if(prop==="tasksPercentage"){
+            let per=parseInt(sub[prop].replace("%",""));
+            if(per<=50)
+                cls="clp1";
+            if(per>50&&per<=75)
+                cls="clp2";
+            if(per>75)
+                cls="clp3";
+        }
+        if(prop==="startedAt"||prop==="finishedAt")
+        {
+            let date=String(sub[prop].getHours())+':'+String(sub[prop].getMinutes());
+            if(sub[prop].getHours()>=0&&sub[prop].getHours()<=9)
+                date="0"+date;
 
-//             if(sub[prop].getMinutes()>=0&&sub[prop].getMinutes()<=9)
-//                 date=date.replace(":",":0");
-
-//             document.write('<td class="'+prop+'">'+date+'<br> ');
-//             }
-//         else if(prop==="tasksGiven")
-//             document.write('<td class="totalTimeSpent '+cls+'">'+sub["totalTimeSpent"]+'<br> ');
-//         else if(prop==="totalTimeSpent")
-//             document.write('<td class="tasksGiven '+cls+'">'+sub["tasksGiven"]+'<br> ');
-//         else
-//             document.write('<td class="'+prop+' '+cls+'">'+sub[prop]+' </td>');
-//     }
-//     document.write('</tr>');
-// }
+            if(sub[prop].getMinutes()>=0&&sub[prop].getMinutes()<=9)
+                date=date.replace(":",":0");
+            td.setAttribute("class",prop);
+            td.innerHTML=date+"<br>";
+        }
+        else if(prop==="tasksGiven"){
+            td.setAttribute("class","totalTimeSpent "+cls);
+            td.innerHTML=sub["totalTimeSpent"]+"<br>";
+        }
+        else if(prop==="totalTimeSpent"){
+            td.setAttribute("class","tasksGiven "+cls);
+            td.innerHTML=sub["tasksGiven"]+"<br>";
+        }
+        else{
+            td.setAttribute("class",prop+" "+cls);
+            td.innerHTML=sub[prop]+"<br>";
+        }
+}
+}
