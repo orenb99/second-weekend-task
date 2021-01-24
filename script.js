dcfd2d90a4aa8c98d36debe605d2405f286c6140
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-24 14:42:50 +0200

    created the table and added topics

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ let subject10={
 
 let arr=[subject1,subject2,subject3,subject4,subject5,subject6,subject7,subject8,subject9,subject10];
 
-
+function totalWork(){
 for(let sub of arr){
             let d=new Date(sub.finishedAt.getTime()-sub.startedAt.getTime());
             let str=d.getHours()-2;
@@ -91,73 +91,86 @@ for(let sub of arr){
             sub.tasksPercentage=Math.round((sub.tasksFinished/sub.tasksGiven)*100)+"%";
             
 }
-
+}
 
 // DOM manipulation
-document.write('<h1 class="heading"> My Subjects Table</h1>');
-document.write('<table id="table">');
-document.write('<tr>');
-for(let prop in arr[0]){
-    let temp="";
-    if(prop==="totalTimeSpent")
-        temp="tasksGiven";
-    else if(prop==="tasksGiven")
-        temp="totalTimeSpent";
-    else
-        temp=prop;
-    let str="";
-    for(let i=0;i<temp.length;i++){
-        str+=temp.charAt(i);
-        if(temp.charAt(i+1)===temp.charAt(i+1).toUpperCase())
-            str+=" ";
-    }
-    
-    document.write('<td class="'+temp+' title">'+str+'</td>');
-}
-document.write('</tr>')
+const h1=document.createElement("h1");
+h1.setAttribute("class","heading");
+h1.innerText="My Subjects Table";
+document.body.append(h1);
 
-for(let sub of arr){
-    let cls="";
-    document.write('<tr>');
-    for(let prop in sub){
-        cls="";
-        if(prop==="tasksGiven"){
-            let hrs=parseInt(sub["totalTimeSpent"]);
-            if(hrs<=2)
-                cls="clg1";
-            if(hrs>2&&hrs<=5)
-                cls="clg2";
-            if(hrs>5)
-                cls="clg3";
-        }
-        if(prop==="tasksPercentage"){
-            let per=parseInt(sub[prop].replace("%",""));
-            if(per<=50)
-                cls="clp1";
-            if(per>50&&per<=75)
-                cls="clp2";
-            if(per>75)
-                cls="clp3";
-        }
-        if(prop==="startedAt"||prop==="finishedAt")
-        {
-            let date=String(sub[prop].getHours())+':'+String(sub[prop].getMinutes());
-            if(sub[prop].getHours()>=0&&sub[prop].getHours()<=9)
-                date="0"+date;
-
-            if(sub[prop].getMinutes()>=0&&sub[prop].getMinutes()<=9)
-                date=date.replace(":",":0");
-
-            document.write('<td class="'+prop+'">'+date+'<br> ');
-            }
+const table=document.createElement("table");
+table.id="table";
+document.body.append(table);
+addTitle();
+function addTitle(){
+    const tr=document.createElement("tr");
+    table.append(tr);
+    for(let prop in arr[0]){
+        let temp="";
+        if(prop==="totalTimeSpent")
+            temp="tasksGiven";
         else if(prop==="tasksGiven")
-            document.write('<td class="totalTimeSpent '+cls+'">'+sub["totalTimeSpent"]+'<br> ');
-        else if(prop==="totalTimeSpent")
-            document.write('<td class="tasksGiven '+cls+'">'+sub["tasksGiven"]+'<br> ');
+            temp="totalTimeSpent";
         else
-            document.write('<td class="'+prop+' '+cls+'">'+sub[prop]+' </td>');
+            temp=prop;
+        let str="";
+        for(let i=0;i<temp.length;i++){
+            str+=temp.charAt(i);
+            if(temp.charAt(i+1)===temp.charAt(i+1).toUpperCase())
+                str+=" ";
+        }
+    const td=document.createElement("td");
+    tr.append(td);
+    td.setAttribute("class","title");
+    td.innerHTML=str;
     }
-    document.write('</tr>');
 }
 
-document.write('</table>');
+    
+//     document.write('<td class="'+temp+' title">'+str+'</td>');
+// }
+
+// for(let sub of arr){
+//     let cls="";
+//     document.write('<tr>');
+//     for(let prop in sub){
+//         cls="";
+//         if(prop==="tasksGiven"){
+//             let hrs=parseInt(sub["totalTimeSpent"]);
+//             if(hrs<=2)
+//                 cls="clg1";
+//             if(hrs>2&&hrs<=5)
+//                 cls="clg2";
+//             if(hrs>5)
+//                 cls="clg3";
+//         }
+//         if(prop==="tasksPercentage"){
+//             let per=parseInt(sub[prop].replace("%",""));
+//             if(per<=50)
+//                 cls="clp1";
+//             if(per>50&&per<=75)
+//                 cls="clp2";
+//             if(per>75)
+//                 cls="clp3";
+//         }
+//         if(prop==="startedAt"||prop==="finishedAt")
+//         {
+//             let date=String(sub[prop].getHours())+':'+String(sub[prop].getMinutes());
+//             if(sub[prop].getHours()>=0&&sub[prop].getHours()<=9)
+//                 date="0"+date;
+
+//             if(sub[prop].getMinutes()>=0&&sub[prop].getMinutes()<=9)
+//                 date=date.replace(":",":0");
+
+//             document.write('<td class="'+prop+'">'+date+'<br> ');
+//             }
+//         else if(prop==="tasksGiven")
+//             document.write('<td class="totalTimeSpent '+cls+'">'+sub["totalTimeSpent"]+'<br> ');
+//         else if(prop==="totalTimeSpent")
+//             document.write('<td class="tasksGiven '+cls+'">'+sub["tasksGiven"]+'<br> ');
+//         else
+//             document.write('<td class="'+prop+' '+cls+'">'+sub[prop]+' </td>');
+//     }
+//     document.write('</tr>');
+// }
